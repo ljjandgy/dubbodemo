8b055b97d7f56baefc3f6cedb613748fe7b62a6b
ljj <junjie.luo@enmotech.com>
2021-05-12 18:27:36 +0800

    init project

---
+++ demo-server/src/main/java/com/example/dubbo/demoserver/DemoServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.dubbo.demoserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoServerApplication.class, args);
+    }
+
+}
